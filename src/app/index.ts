a986e7ab514b2f54119a6d0e2fb1fc776262fc3e
mahipal <mahipalsinh.thakor@gmail.com>
2026-06-23 19:53:08 +0530

    signup done

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import type { Express } from "express";
+import { authRouter } from "./auth/routes.js";
 
 export function createApplication(): Express {
   const app = express();
 
   // Middleware
+  app.use(express.json());
 
   // routes
   app.get("/health", (req, res) => {
     res.json({ message: "health route test" });
   });
+
+  app.use("/auth", authRouter);
 
   return app;
 }
