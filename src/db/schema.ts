4cfb657e86131723167fff60673b9df7b84f8ed5
mahipal <mahipalsinh.thakor@gmail.com>
2026-06-23 17:50:47 +0530

    drizzle setup

---
+++ src/db/schema.ts
@@ -0,0 +1,27 @@
+// tables
+
+import {
+  integer,
+  pgTable,
+  varchar,
+  uuid,
+  boolean,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  //   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  //   name: varchar({ length: 255 }).notNull(),
+  //   age: integer().notNull(),
+  //   email: varchar({ length: 255 }).notNull().unique(),
+  id: uuid("id").primaryKey().defaultRandom(),
+  firstName: varchar("first_name", { length: 45 }).notNull(),
+  lastName: varchar("last_name", { length: 45 }),
+  email: varchar("email", { length: 322 }).notNull().unique(),
+  emailVerified: boolean("email_verified").default(false).notNull(),
+  password: varchar("password", { length: 66 }),
+  salt: text("salt"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+});
